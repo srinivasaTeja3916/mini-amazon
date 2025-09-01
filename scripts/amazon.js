6812a2909dd84412b915f9381edaa97e2740ed9e
teja <svstejasajja@gmail.com>
2025-09-01 10:13:49 +0530

    Make add to cart button interactve

---
+++ scripts/amazon.js
@@ -0,0 +1,75 @@
+let htmlText = "";
+products.forEach((product) => {
+  htmlText += `
+  <div class="product-container">
+    <div class="product-image-container">
+      <img
+        class="product-image"
+        src="${product.image}"
+      />
+    </div>
+
+    <div class="product-name limit-text-to-2-lines">
+      ${product.name}
+    </div>
+
+    <div class="product-rating-container">
+      <img class="product-rating-stars" src="images/ratings/rating-${
+        product.rating.stars * 10
+      }.png" />
+      <div class="product-rating-count link-primary">${
+        product.rating.count
+      }</div>
+    </div>
+
+    <div class="product-price">$${product.priceCents / 100}</div>
+    <div class="product-quantity-container">
+      <select name="amazonName">
+        <option selected value="1">
+          1
+        </option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </select>
+    </div>
+
+    <div class="product-spacer"></div>
+
+    <div class="added-to-cart">
+      <img src="images/icons/checkmark.png" />
+      Added
+    </div>
+
+    <button class="add-to-cart-button button-primary">Add to Cart</button>
+  </div>`;
+});
+document.querySelector(".products-grid").innerHTML = htmlText;
+document.querySelectorAll(".button-primary").forEach((element, i) => {
+  element.addEventListener("click", () => {
+    let present;
+    cart.forEach((ele) => {
+      if (ele.productId === products[i].id) {
+        present = ele;
+      }
+    });
+    if (present) {
+      present.quatity += 1;
+    } else {
+      cart.push({
+        productId: products[i].id,
+        quatity: 1,
+      });
+    }
+    console.log(cart);
+  });
+});
+// for (let i = 0; i < products.length; i++) {
+//   console.log(document.querySelectorAll(".button-primary"));
+// }
