7bcd27510426f7550bd7d3f8c4ba265ed9b99aa3
teja <svstejasajja@gmail.com>
2025-09-05 15:58:13 +0530

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,17 @@
-export const cart = [];
+export const cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 1,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+  },
+  {
+    productId: "54e0eccd-8f36-462b-b68a-8182611d9add",
+    quantity: 3,
+  },
+];
 export function addToCart(productId) {
   let quantity = Number(
     document.querySelector(`.js-quantity-selector-${productId}`).value
