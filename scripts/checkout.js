a5d9d8ccb452fcf2991f8fe99edf587dd42b7e7e
teja <svstejasajja@gmail.com>
2025-09-17 14:33:59 +0530

    learn promises and convert code into promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,46 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
-// import "../data/products.js";
-loadProducts(() => {
+import { loadCart } from "../data/cart.js";
+import "../data/products.js";
+
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve("I'm Teja");
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve("I'm Yugandhar<-->I'm Balu");
+    });
+  }),
+]).then((value) => {
+  console.log(value);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+// new Promise((resolve) => {
+//   loadProducts(() => {
+//     resolve();
+//   });
+// })
+//   .then(() => {
+//     return new Promise((resolve) => {
+//       loadCart(() => {
+//         resolve();
+//       });
+//     });
+//   })
+//   .then(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   });
+
+// loadProducts(() => {
+//   loadCart(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   });
+// });
