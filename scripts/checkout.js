e24f008d2b8ab169b333fd00fa868c1894fcfdbf
teja <svstejasajja@gmail.com>
2025-09-17 15:53:52 +0530

    Convert loadProducts from XHR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,46 +1,18 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 import "../data/products.js";
 
 Promise.all([
-  new Promise((resolve) => {
-    loadProducts(() => {
-      resolve("I'm Teja");
-    });
-  }),
+  loadProductsFetch(),
   new Promise((resolve) => {
     loadCart(() => {
-      resolve("I'm Yugandhar<-->I'm Balu");
+      resolve();
     });
   }),
-]).then((value) => {
-  console.log(value);
+]).then(() => {
+  console.log();
   renderOrderSummary();
   renderPaymentSummary();
 });
-
-// new Promise((resolve) => {
-//   loadProducts(() => {
-//     resolve();
-//   });
-// })
-//   .then(() => {
-//     return new Promise((resolve) => {
-//       loadCart(() => {
-//         resolve();
-//       });
-//     });
-//   })
-//   .then(() => {
-//     renderOrderSummary();
-//     renderPaymentSummary();
-//   });
-
-// loadProducts(() => {
-//   loadCart(() => {
-//     renderOrderSummary();
-//     renderPaymentSummary();
-//   });
-// });
