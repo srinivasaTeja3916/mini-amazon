ce6d7685c5827797df6951dfab883258905ce9b6
teja <svstejasajja@gmail.com>
2025-09-19 18:24:24 +0530

    all detaills are set

--- data/orders.js
+++ data/orders.js
@@ -1,4 +1,4 @@
-export const orders = localStorage.getItem("orders") || [];
+export const orders = JSON.parse(localStorage.getItem("orders")) || [];
 
 export function addOrder(order) {
   orders.unshift(order);
